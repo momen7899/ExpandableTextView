76ab1794ea593bda3e28fa71c672f2986ffda4fd
Momen Hamaveysi <momen.hamaveysi@kubakgroup.com>
2021-09-23 17:41:21 +0330

    Complete First version

---
+++ src/main/java/com/momen/expandabletextview/ExpandableTextView.kt
@@ -0,0 +1,36 @@
+package com.momen.expandabletextview
+
+import android.content.Context
+import android.util.AttributeSet
+
+class ExpandableTextView :
+    androidx.appcompat.widget.AppCompatTextView {
+
+
+    constructor(context: Context) : super(context)
+
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
+
+
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) :
+            super(context, attrs, defStyleAttr)
+
+
+    public fun setExpandable(isExpandable: Boolean) {
+        if (isExpandable) {
+            expand()
+        } else {
+            notExpand()
+        }
+    }
+
+    private fun expand() {
+        maxLines = Integer.MAX_VALUE
+    }
+
+    private fun notExpand() {
+        maxLines = 1
+    }
+
+
+}
